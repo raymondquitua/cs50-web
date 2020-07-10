391ef284bbbd666b58f5218d70d4cd565f8e0604
Raymond <50416517+raymondquitua@users.noreply.github.com>
2020-07-10 17:47:24 +0800

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int x = 25;
+    int y = 50;
+
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+
+    //swapping
+    
+    printf("x is now %i", x);
+    printf("y is now %i", y);
+}
